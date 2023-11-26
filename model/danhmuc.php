aa5b2d879057d43d81b8b27f26b16000737fe210
thuongnguyen19 <150455273+thuongnguyen19@users.noreply.github.com>
2023-11-27 02:21:15 +0700

    cmtt

--- model/danhmuc.php
+++ model/danhmuc.php
@@ -14,3 +14,35 @@ function load_ten_danhmuc($iddm){
         return "";
     }
 }
+// lấy 1 danh mục
+function loadone_danhmuc($id_danh_muc){
+    $sql="select *from danh_muc_san_pham where id_danh_muc=  ".$id_danh_muc;
+    $dm=pdo_query_one($sql);
+    return $dm;
+}
+function insert_danhmuc($tenloai)
+{
+    $sql = "INSERT INTO danh_muc_san_pham(name) VALUES('$tenloai')";
+    pdo_execute($sql);
+}
+function delete_danhmuc($id_danh_muc)
+{
+    $sql = "DELETE FROM danh_muc_san_pham where id_danh_muc =".$id_danh_muc;
+    pdo_query($sql);
+}
+function update_danhmuc($id_danh_muc, $tenloai)
+{
+    $sql = "UPDATE danh_muc_san_pham SET name='" . $tenloai . "' WHERE id_danh_muc=" . $id_danh_muc;
+    pdo_execute($sql);
+}
+// function load_ten_danhmuc($iddm)
+// {
+//     if ($iddm > 0) {
+//         $sql = "SELECT * FROM danh_muc_san_pham where id_danh_muc = " . $iddm;
+//         $dm =  pdo_query_one($sql);
+//         extract($dm);
+//         return $name;
+//     } else {
+//         return "";
+//     }
+// }
